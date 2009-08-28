c42a8b9d677cbceae90deda189076c92aed7207a
mark <mark@19484d66-ee1e-0410-975a-9de0e5f923a4>
2009-08-28 21:41:33 +0000

    version 1.1 released
    git-svn-id: https://server.devisland.net/mark/repos/RTExtensionMenubarUserTickets@418 19484d66-ee1e-0410-975a-9de0e5f923a4

--- lib/RT/Extension/MenubarUserTickets.pm
+++ lib/RT/Extension/MenubarUserTickets.pm
@@ -3,19 +3,31 @@ use strict;
 use 5.8.3;	#this is what RT requires http://wiki.bestpractical.com/view/ManualRequirements
 package RT::Extension::MenuBarUserTickets;
 
-our $VERSION = '1.0';
+our $VERSION = '1.1';
 
 return 1;
 
 =pod
 
-=head1 Author
+=head1 NAME
+
+RT::Extension::MenuBarUserTickets - List tickets belonging to a specific user
+
+=head1 DESCRIPTION
+
+Adds an additional button and dropdown list box showing every enabled user
+to the top of the RT interface page.
+
+After selecting the required user from the list and clicking the show tickets
+button query results will be should in the normal manner.
+
+=head1 AUTHOR
 
 MacGyveR <dumb@cpan.org>
 
 Development questions, bug reports, and patches are welcome to the above address
 
-=head1 Copyright
+=head1 COPYRIGHT
 
 Copyright (c) 2009 MacGyveR. All rights reserved.
 
