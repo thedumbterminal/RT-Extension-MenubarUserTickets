3f50cb3d4a0f6958892593b5fdc8b76f7e3b7a3d
mark <mark@19484d66-ee1e-0410-975a-9de0e5f923a4>
2009-08-28 21:00:36 +0000

    Initial import.
    git-svn-id: https://server.devisland.net/mark/repos/RTExtensionMenubarUserTickets@417 19484d66-ee1e-0410-975a-9de0e5f923a4

---
+++ lib/RT/Extension/MenubarUserTickets.pm
@@ -0,0 +1,24 @@
+use warnings;
+use strict;
+use 5.8.3;	#this is what RT requires http://wiki.bestpractical.com/view/ManualRequirements
+package RT::Extension::MenuBarUserTickets;
+
+our $VERSION = '1.0';
+
+return 1;
+
+=pod
+
+=head1 Author
+
+MacGyveR <dumb@cpan.org>
+
+Development questions, bug reports, and patches are welcome to the above address
+
+=head1 Copyright
+
+Copyright (c) 2009 MacGyveR. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
